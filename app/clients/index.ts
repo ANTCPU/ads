520d6ad63f306e010304e5d8b23bfa60929262e2
Antony Ciccone <antony@antcpu.com>
2026-07-08 23:00:06 -0400

    Export additional assets and types from mapofpi

--- app/clients/index.ts
+++ app/clients/index.ts
@@ -4,6 +4,8 @@
 // ============================================================
 
 export { default as mapofpi, MAPOFPI_KB, ADS_SYSTEM_PROMPT as MAPOFPI_PROMPT } from './mapofpi/kb';
+export { MAPOFPI_ICONS, MAPOFPI_VIDEOS, MAPOFPI_PHASES, MAPOFPI_COUNTRIES } from './mapofpi/assets';
+export type { ShopIcon, MapOfPiVideo, Phase, Country } from './mapofpi/assets';
 
 // Future clients:
 // export { default as clientname } from './clientname/kb';
